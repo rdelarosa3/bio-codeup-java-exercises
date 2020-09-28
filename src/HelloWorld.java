d379b4855971dd67a9d5f2ebd229f48d5a178f94
Robert De la Rosa <rdelarosa39@gmail.com>
2020-09-28 13:58:07 -0500

    Add: HelloWorld Java file

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, ");
+        System.out.println("World!");
+
+        int myFavoriteNumber = 7;
+        String myString = "this is my string";
+        System.out.println(myString);
+//        myString = 'c';
+//        myString = 3.14159;
+        float myNumber = 3.14f;
+        System.out.println(myString);
+//        long myNumber = 3.14;
+//        long myNumber = 123L;
+        System.out.println(myNumber);
+        myNumber = 123;
+
+        int y = 5;
+        System.out.println(y++);
+        System.out.println(y);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String class = "hello";
+    }
+}
