8684b5591016c2297599204d3283a573389da9aa
Robert De la Rosa <rdelarosa39@gmail.com>
2020-10-01 15:29:03 -0500

    Update: added continue statement

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,14 +5,16 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         /* 1. LOOP BASICS */
         System.out.println(ANSI_GREEN + "1. LOOP BASICS EXERCISE" + ANSI_RESET);
-        /* WHILE */
+
+        /* 1a WHILE */
         System.out.println(ANSI_YELLOW+"1a While"+ANSI_RESET);
         int i = 5;
         while (i <= 15){
             System.out.println(i);
             i++;
         }
-        /* DO WHILE */
+
+        /* 1b DO WHILE */
         System.out.println(ANSI_YELLOW+"1b Do While"+ANSI_RESET);
         System.out.println(ANSI_PURPLE+"  1b FIRST:"+ANSI_RESET);
         i = 0;
@@ -31,7 +33,8 @@ public class ControlFlowExercises {
             System.out.println(j);
             j *= j;
         }while (j < 1000000);
-        /* 1c for loop */
+
+        /* 1c FOR LOOP */
         System.out.println(ANSI_YELLOW+"1c For Loop"+ANSI_RESET);
         System.out.println(ANSI_PURPLE+"  1c FIRST:"+ANSI_RESET);
         for (long k = 2L; k < 1000000; k*=k) {
@@ -49,6 +52,7 @@ public class ControlFlowExercises {
         for (int k = 100; k >=-10 ; k-=5) {
             System.out.println(k);
         }
+
         /* 2. FIZZBUZZ */
         System.out.println(ANSI_GREEN+ "2. FIZZBUZZ EXERCISE"+ANSI_RESET);
         for (int k = 0; k <= 100 ; k++) {
@@ -60,65 +64,95 @@ public class ControlFlowExercises {
                 System.out.println(ANSI_CYAN+"Fizz"+ANSI_RESET);
             }
         }
+
         /* 3. Display Power of Tables */
         System.out.println(ANSI_GREEN+"3. DISPLAY TABLE OF POWERS"+ANSI_RESET);
-        Scanner scanner = new Scanner(System.in);
+        Scanner tableScanner = new Scanner(System.in);
         int userInput = 0;
-        while(userInput == 0){
-            System.out.println(ANSI_BLUE+"  What number would you like to go up to?"+ANSI_RESET);
-            boolean hasNextInt = scanner.hasNextInt();
-            if(hasNextInt){
-                userInput = scanner.nextInt();
-                System.out.println("number | squared | cubed");
-                System.out.println("------ | ------- | -----");
-
-                for (int k = 0; k <= userInput ; k++) {
-                    int k2 = k*k;
-                    int k3 = k*k*k;
-                    String str1 = String.format("%-7s",k);
-                    String str2 = String.format("%-8s",k2);
-                    String str3 = String.format("%-5s",k3);
-                    String strLine = str1 + "| " + str2 + "| " + str3;
-                    System.out.println(strLine);
+        while (userInput <= 0) {
+            System.out.println(ANSI_BLUE + "  What number would you like to go up to?" + ANSI_RESET);
+            boolean hasNextInt = tableScanner.hasNextInt();
+            if (hasNextInt) {
+                userInput = tableScanner.nextInt();
+                if(userInput >=0){
+                    // continue asking user to continue until yes or no reply
+                    while(true){
+                        System.out.println(ANSI_BLUE+"Continue? [Y/n]"+ANSI_RESET);
+                        Scanner sc = new Scanner(System.in);
+                        String userResponse = sc.nextLine();
+                        // if reply is yes print table of powers
+                        if(userResponse.toLowerCase().startsWith("y")) {
+                            System.out.println(ANSI_YELLOW + "number | squared | cubed");
+                            System.out.println("------ | ------- | -----" + ANSI_RESET);
+                            for (int k = 0; k <= userInput; k++) {
+                                int k2 = k * k;
+                                int k3 = k * k * k;
+                                String str1 = String.format("%-7s", k);
+                                String str2 = String.format("%-8s", k2);
+                                String str3 = String.format("%-5s", k3);
+                                String strLine = str1 +
+                                        ANSI_YELLOW + "| " + ANSI_RESET + str2 +
+                                        ANSI_YELLOW + "| " + ANSI_RESET + str3;
+                                System.out.println(strLine);
+                            }
+                            break;
+                        }
+                        else if(userResponse.toLowerCase().startsWith("n")){
+                            break;
+                        }
+                    }
                 }
-            }else{
-                System.out.println(ANSI_RED+"Invalid Input"+ANSI_RESET);
+            } else {
+                System.out.println(ANSI_RED + "Invalid Input" + ANSI_RESET);
             }
-            scanner.nextLine();
+            tableScanner.nextLine();
         }
+
+
         /* 4 CONVERT GRADES */
         System.out.println(ANSI_GREEN+"4. CONVERT GRADES"+ANSI_RESET);
-        scanner = new Scanner(System.in);
+        Scanner gradeScanner = new Scanner(System.in);
         int grade = -1;
-        while(grade < 0){
-            System.out.println(ANSI_BLUE+"  Enter Grade 0-100:"+ANSI_RESET);
-            boolean hasNextInt = scanner.hasNextInt();
-            if(hasNextInt){
-                grade = scanner.nextInt();
-                if(grade>98 && grade<101){
-                    System.out.println("A+");
+        while (grade < 0 || grade >100 ) {
+            System.out.println(ANSI_BLUE + "  Enter Grade 0-100:" + ANSI_RESET);
+            boolean hasNextInt = gradeScanner.hasNextInt();
+            if (hasNextInt) {
+                grade = gradeScanner.nextInt();
+                if(grade >= 0 && grade <=100){
+                    while(true) {
+                        System.out.println(ANSI_BLUE+"Continue? [Y/n]"+ANSI_RESET);
+                        Scanner sc = new Scanner(System.in);
+                        String userResponse = sc.nextLine();
+                        if (userResponse.toLowerCase().startsWith("y")) {
+                            if (grade > 98 && grade < 101) {
+                                System.out.println("A+");
+                            } else if (grade >= 88 && grade < 99) {
+                                System.out.println("A");
+                            } else if (grade >= 79 && grade < 88) {
+                                System.out.println("B");
+                            } else if (grade >= 67 && grade < 79) {
+                                System.out.println("C");
+                            } else if (grade >= 60 && grade < 67) {
+                                System.out.println("D");
+                            } else {
+                                System.out.println("F");
+                            }
+                            break;
+                        }
+                        else if(userResponse.toLowerCase().startsWith("n")){
+                            break;
+                        }
+                    }
                 }
-                else if(grade>=88 && grade<99){
-                    System.out.println("A");
-                }
-                else if(grade>=79 && grade<88){
-                    System.out.println("B");
-                }
-                else if(grade>=67 && grade<79) {
-                    System.out.println("C");
-                }
-                else if(grade>=60 && grade<67){
-                    System.out.println("D");
-                }else{
-                    System.out.println("F");
-                }
-            }else{
-                System.out.println(ANSI_RED+"Invalid Input"+ANSI_RESET);
+            } else {
+                System.out.println(ANSI_RED + "Invalid Input" + ANSI_RESET);
             }
-            scanner.nextLine();
+            gradeScanner.nextLine();
         }
 
+
     }
+    /* ANSI COLORS TO USE IN TERMINAL PRINTOUT */
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
